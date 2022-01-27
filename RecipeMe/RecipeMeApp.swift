fbc398a23035c0a388a7966e24df4de71cdc52a6
gv4383 <gregory.vargas18@gmail.com>
2022-01-26 21:02:26 -0600

    Initial Commit

---
+++ RecipeMe/RecipeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeMeApp.swift
+//  RecipeMe
+//
+//  Created by Goyo Vargas on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
