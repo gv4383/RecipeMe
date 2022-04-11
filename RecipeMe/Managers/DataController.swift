bd504d3d1185e0129313996411541a4766ae5dc3
gv4383 <gregory.vargas18@gmail.com>
2022-04-10 22:39:04 -0500

    Add preview instance of DataController for SwiftUI previews

--- RecipeMe/Managers/DataController.swift
+++ RecipeMe/Managers/DataController.swift
@@ -11,9 +11,41 @@ import Foundation
 final class DataController: ObservableObject {
     let container: NSPersistentContainer
     static let shared = DataController()
+    static var preview: DataController = {
+        let result = DataController(inMemory: true)
+//        let viewContext = result.container.viewContext
+        
+//        for _ in 0..<10 {
+//            let newRecipe = Recipe(context: viewContext)
+//            newRecipe.createdAt = Date()
+//            newRecipe.id = UUID()
+//            newRecipe.ingredients = "Test ingredient"
+//            newRecipe.name = "Testz"
+//            newRecipe.recipeDescription = "Test description"
+//            newRecipe.steps = "Test step"
+//            newRecipe.totalTime = 8
+//        }
+//
+//        do {
+//            try viewContext.save()
+//        } catch {
+//            // Replace this implementation with code to handle the error appropriately.
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            let nsError = error as NSError
+//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//        }
+        
+        return result
+    }()
     
-    private init() {
+    
+    private init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "RecipeMeModel")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
         container.loadPersistentStores { description, error in
             if let error = error {
                 print("Core Data failed to load: \(error.localizedDescription)")
