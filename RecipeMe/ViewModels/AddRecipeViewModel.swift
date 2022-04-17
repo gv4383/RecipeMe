5da757a94e30dca7b622e5429a0eb873f52b2d19
gv4383 <gregory.vargas18@gmail.com>
2022-04-17 16:10:42 -0500

    Create addNewRecipe method in AddRecipeViewModel

--- RecipeMe/ViewModels/AddRecipeViewModel.swift
+++ RecipeMe/ViewModels/AddRecipeViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Goyo Vargas on 4/15/22.
 //
 
+import CoreData
 import Foundation
 
 final class AddRecipeViewModel: ObservableObject {
@@ -13,4 +14,14 @@ final class AddRecipeViewModel: ObservableObject {
     @Published var ingredients = ""
     @Published var steps = ""
     @Published var time = 0
+    
+    private let context: NSManagedObjectContext
+    
+    init() {
+        context = DataController.shared.container.viewContext
+    }
+    
+    func addNewRecipe() {
+        print("New recipe (\(name)) added!")
+    }
 }
