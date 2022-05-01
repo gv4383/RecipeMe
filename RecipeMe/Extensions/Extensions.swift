54222c3dc69cb1bd1e5faa1048251eb82b67a504
gv4383 <gregory.vargas18@gmail.com>
2022-05-01 16:55:54 -0500

    Create extensions for easily rounding out corners in UI

---
+++ RecipeMe/Extensions/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  RecipeMe
+//
+//  Created by Goyo Vargas on 5/1/22.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        
+        return Path(path.cgPath)
+    }
+}
