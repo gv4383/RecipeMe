5ffcd15a85ef8dbce44177eeaaac5e6b1efe1c34
gv4383 <gregory.vargas18@gmail.com>
2022-05-26 09:45:18 -0500

    Move create test recipe and create recipe buttons to toolbar

--- RecipeMe/Views/MenuView.swift
+++ RecipeMe/Views/MenuView.swift
@@ -16,39 +16,48 @@ struct MenuView: View {
     ]
     
     var body: some View {
-        VStack {
-            Button("Add test recipe") {
-                print("Creating new recipe...")
-                let context = DataController.shared.container.viewContext
-                let recipe = Recipe(context: context)
-                recipe.createdAt = Date()
-                recipe.id = UUID()
-                recipe.name = "Fried Rice"
-                recipe.totalTime = 30
-                recipe.recipeDescription = "A simple and quick recipe for nights where you don't feel like spending too much time on cooking."
-                recipe.ingredients = "Cooking oil as needed\n2 cups of pre-cooked rice (1-2 day old cooked rice preferred)\n1 cup of pork\n1 quarter to half an onion\n3 cloves of garlic\n1 cup of carrots\n1 egg\n1 tsp of soy sauce\n1 tsp of fish sauce\n1 tsp of black soy sauce\n1 tsp of sugar"
-                recipe.steps = "Heat wok/pan over medium-high heat\nCut pork into bite-size pieces\nPour oil into wok/pan, toss pork in, and cook until one side is browned\nRemove meat from wok/pan\nDice onions, garlic, and carrots\nAdd more oil along with onions and garlic\nCook onions/garlic for a few minutes\nPut egg in and scramble\nAdd carrots and cook for a minute or 2\nAdd rice to wok/pan and break it apart\nAdd meat back in and mix everything together\nAdd soy sauce, fish sauce, black soy sauce, and sugar\nMix everything together and cook for 3-5 minutes\nServe on plate and eat"
-                
-                do {
-                    try context.save()
-                } catch {
-                    print("Error saving recipe: \(error)")
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(viewModel.recipes) { recipe in
+                    NavigationLink {
+                        RecipeView(recipe: recipe)
+                    } label: {
+                        RecipeCardView(recipe: recipe)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    print("Creating new recipe...")
+                    let context = DataController.shared.container.viewContext
+                    let recipe = Recipe(context: context)
+                    recipe.createdAt = Date()
+                    recipe.id = UUID()
+                    recipe.name = "Fried Rice"
+                    recipe.totalTime = 30
+                    recipe.recipeDescription = "A simple and quick recipe for nights where you don't feel like spending too much time on cooking."
+                    recipe.ingredients = "Cooking oil as needed\n2 cups of pre-cooked rice (1-2 day old cooked rice preferred)\n1 cup of pork\n1 quarter to half an onion\n3 cloves of garlic\n1 cup of carrots\n1 egg\n1 tsp of soy sauce\n1 tsp of fish sauce\n1 tsp of black soy sauce\n1 tsp of sugar"
+                    recipe.steps = "Heat wok/pan over medium-high heat\nCut pork into bite-size pieces\nPour oil into wok/pan, toss pork in, and cook until one side is browned\nRemove meat from wok/pan\nDice onions, garlic, and carrots\nAdd more oil along with onions and garlic\nCook onions/garlic for a few minutes\nPut egg in and scramble\nAdd carrots and cook for a minute or 2\nAdd rice to wok/pan and break it apart\nAdd meat back in and mix everything together\nAdd soy sauce, fish sauce, black soy sauce, and sugar\nMix everything together and cook for 3-5 minutes\nServe on plate and eat"
+                    
+                    do {
+                        try context.save()
+                    } catch {
+                        print("Error saving recipe: \(error)")
+                    }
+                } label: {
+                    Label("Fast Create", systemImage: "bolt.fill")
+                        .labelStyle(.iconOnly)
                 }
             }
             
-            Button("Add new recipe") {
-                viewModel.isShowingAddRecipeView = true
-            }
-            
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(viewModel.recipes) { recipe in
-                        NavigationLink {
-                            RecipeView(recipe: recipe)
-                        } label: {
-                            RecipeCardView(recipe: recipe)
-                        }
-                    }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    viewModel.isShowingAddRecipeView = true
+                } label: {
+                    Label("Create", systemImage: "plus")
+                        .labelStyle(.iconOnly)
                 }
             }
         }
